7f559adb408da4dd1ca5c88636aa4f49691ffaac
falcon0125 <falcon0125@gmail.com>
2015-12-15 23:33:18 +0800

    with new geocodeing python

---
+++ geo.py
@@ -0,0 +1,41 @@
+#!/usr/bin/python
+#-*- coding: UTF8 -*-
+
+import pandas as pd
+import geocoder
+import time
+df = pd.read_csv('hospbsc.csv',encoding='UTF8')
+df.columns=['area','id','name','address','phone-code', 'phone', 'level', 'code','type','close_date' ]
+df = df[df.level.isin([u'1',u'2',u'3',u'4'])] #只算醫療院所
+
+addr= df.address.tolist()
+hid = df.id.tolist()
+addr_list = zip(hid, addr)
+
+print 'size of df : ',len(addr_list)
+
+def div(l, n):
+    N = len(l)
+    g = int(N/n)
+    r = []
+    for i in range(g+1):
+        r.append(l[i*n:n*(i+1)+1])
+    return r
+
+result =[]
+to_solve = div(addr_list, 2400)[0]
+
+
+for entity in to_solve:
+    hid,addr = entity
+    g = geocoder.api.Google(addr)
+    d = dict()
+    d['hid'] = hid
+    d['confidence'] = g.confidence
+    d['lat'] = g.lat
+    d['lng'] = g.lng
+    d['address'] = addr
+    d['status'] = g.status
+    print repr(d)
+    result.append(d)
+    time.sleep(1)
